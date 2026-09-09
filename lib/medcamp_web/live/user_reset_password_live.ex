14dfc6ebb318b46089484df75d0cb096fa4b5919
mmunavu <michaelmunavu83@gmail.com>
2026-09-09 06:55:25 +0300

    Small Change

--- lib/medcamp_web/live/user_reset_password_live.ex
+++ lib/medcamp_web/live/user_reset_password_live.ex
@@ -15,7 +15,7 @@ defmodule MedcampWeb.UserResetPasswordLive do
         phx-change="validate"
       >
         <.error :if={@form.errors != []}>
-          Oops, something went wrong! Please check the errors below.
+          Oops,  something went wrong! Please check the errors below.
         </.error>
 
         <.input field={@form[:password]} type="password" label="New password" required />
